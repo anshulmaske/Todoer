0d695d08d99684bc122fbf9fbadcfa5096e4c372
Anshul M <anshmaske743@gmail.com>
2021-10-10 20:11:58 -0400

    Added support for SQLite | fixed gitignore

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy, sqlalchemy
 
 app =Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite_test'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
     return render_template('homepage.html')
 
-if '__name__' == '__main__':
+if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
